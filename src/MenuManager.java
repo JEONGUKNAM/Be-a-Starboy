cf801e1a5e31f2f46bfbfd95fd67370b2afe5395
NAMJEONGUK <sheeld123@naver.com>
2021-03-19 15:59:41 +0900

    Edit several menus

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -12,7 +12,7 @@ public class MenuManager {
 		System.out.println("1. Add Students");
 		System.out.println("2. Your bad habits");
 		System.out.println("3. Edit Strength");
-		System.out.println("4. View Students");
+		System.out.println("4. Things you want to do");
 		System.out.println("5. Show a Menu");
 		System.out.println("6. Exit");
 		System.out.println("Select one number between 1-6:");
@@ -27,6 +27,7 @@ public class MenuManager {
 			String studentemailaddress = input.next();
 			System.out.print("Student phone number: ");
 			String studentphonenumber = input.next();
+			continue;
 		}
 		else if (num ==2)
 		{
@@ -45,6 +46,8 @@ public class MenuManager {
 			String badhabit2 = input.nextLine();
 			System.out.print("number 3 : ");
 			String badhabit3 = input.nextLine();
+			break;
+			
 			}
 			else
 			{
@@ -53,7 +56,7 @@ public class MenuManager {
 		}
 		else if (num ==3)
 		{
-			System.out.println("Do you do for your strengths? (Y/N) :");
+			System.out.println("What do you do to raise your strengths? (Y/N) :");
 			Scanner c = new Scanner(System.in);
 			char a = c.next().charAt(0);
 			
@@ -74,29 +77,40 @@ public class MenuManager {
 		}
 		else if (num ==4)
 		{
-			System.out.println("Student ID :");
-			int studentID = input.nextInt();
-			System.out.print("Student Name: ");
-			String studentName = input.next();
+			String studentID = input.nextLine();
+			System.out.print("number 1 : ");
+			String strength1 = input.nextLine();
+			System.out.print("number 2 : ");
+			String strength2 = input.nextLine();
+			System.out.print("number 3 : ");
+			String strength3 = input.nextLine();
+			break;
 		}
 		else if (num ==5)
 		{
-			System.out.println("Student ID :");
-			int studentID = input.nextInt();
-			System.out.print("Student Name: ");
-			String studentName = input.next();
+			System.out.println("You want to go back to menu again?? (Y/N)");
+			Scanner d= new Scanner(System.in);
+		    char w = d.next().charAt(0);
+		    
+		    if (('Y'==w) || ('y'==w))
+		    {
+                   continue;
+		    }
+		    else
+		    {
+		    	break;
+		    }
+					
+					  
 		}
 		else if (num ==6)
 		{
-			System.out.println("Student ID :");
-			int studentID = input.nextInt();
-			System.out.print("Student Name: ");
+			System.out.println("If you wanna go out? Press any key :");
 			String studentName = input.next();
+			System.out.print("GOOD BYE");
+			break;
 		}
-		else 
-		{
-			System.out.println("올바른 숫자를 입력하세요!");
-		}
+		
 			
 		}
 		}
